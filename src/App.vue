bf668c13708fa86c66ac987e9ef73b533ca3dda1
yll0910 <1092793382@qq.com>
2020-02-08 17:13:11 +0800

    第一次本地提交注释

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Description: 
+ * @Autor: yllz
+ * @Date: 2020-02-08 14:30:55
+ * @LastEditors  : yllz
+ * @LastEditTime : 2020-02-08 15:48:47
+ -->
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+};
+</script>
+
+<style>
+</style>
